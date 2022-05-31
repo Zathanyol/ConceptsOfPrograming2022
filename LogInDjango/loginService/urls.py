81db53fa4602addb744bc4a91a99fa7943cc7410
skittlemassacare <zombiedude723@gmail.com>
2022-05-31 00:36:35 -0700

    done for night

--- LogInDjango/loginService/urls.py
+++ LogInDjango/loginService/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('', views.createUser, name='createUser'),
+    path('inside', views.inside, name='inside'),
+    path('logout', views.userLogout, name='logout'),
 ]
--- LogInDjango/loginService/views.py
+++ LogInDjango/loginService/views.py
@@ -1,15 +1,54 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
-def index(request):
+def createUser(request):
+
     form = UserCreationForm()
 
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Howdy doody!")
+            return redirect("inside")
+
+
     context = {"form": form}
     return render(request, "loginService/register.html", context)
+
+
+def login(request):
+
+    if request.user.is_authenticated:
+        return redirect("inside")
+
+    else:
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password =request.POST.get("password")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+    context = {}
+    return render(request, "loginService/login.html", context)
+
+
+def inside(request):
+    return render(request, "loginService/inside.html")
+
+def userLogout(request):
+    logout(request)
+    return render(request, "loginService/login.html")
